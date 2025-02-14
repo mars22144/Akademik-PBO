6e8774bf656b1764032dcb1481aaf41d0bea48f4
mars22144 <134260139+mars22144@users.noreply.github.com>
2025-02-14 16:36:00 +0700

    Add files via upload

---
+++ akademik (1).sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 14, 2025 at 10:35 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabelmahasiswa`
+--
+
+CREATE TABLE `tabelmahasiswa` (
+  `nim` char(7) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `jurusan` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tabelmahasiswa`
+--
+
+INSERT INTO `tabelmahasiswa` (`nim`, `nama`, `jurusan`) VALUES
+('23456', 'qwert', 'RPL');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tabeluser`
+--
+
+CREATE TABLE `tabeluser` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tabeluser`
+--
+
+INSERT INTO `tabeluser` (`username`, `password`) VALUES
+('aub', 'java');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tabelmahasiswa`
+--
+ALTER TABLE `tabelmahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
